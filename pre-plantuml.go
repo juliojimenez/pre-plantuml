5af728ffffed5d36edf34581229da4c862bb643c
Julio Jimenez <julio@julioj.com>
2021-01-16 22:11:08 -0500

    readme

--- pre-plantuml.go
+++ pre-plantuml.go
@@ -5,16 +5,34 @@ import (
 	"os"
 	"io/ioutil"
 	"log"
+	"path/filepath"
+	"regexp"
 )
 
-func main() {
-	args := os.Args[1:]
-	for _, file := range args {
-		fmt.Println(file)
+func findFiles(re string) []string {
+	libRegEx, e := regexp.Compile(re)
+	if e != nil {
+		log.Fatal(e)
 	}
+	
+	var files []string
+	e = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err == nil && libRegEx.MatchString(info.Name()) {
+			files = append(files, info.Name())
+		}
+		return nil
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
+	return files
+}
+
+func main() {
 	content, err := ioutil.ReadFile("README.md")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("File contents: ", string(content))
+	fmt.Println(findFiles(".*\\.(pu)"))
 }
