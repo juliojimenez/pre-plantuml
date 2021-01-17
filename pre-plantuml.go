10b1647b103d41fbeab04b70d9a2499c267806fb
Julio Jimenez <julio@julioj.com>
2021-01-17 12:25:30 -0500

    more functions

--- pre-plantuml.go
+++ pre-plantuml.go
@@ -8,6 +8,8 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"encoding/hex"
+	"strings"
 )
 
 func findFiles(re string) []string {
@@ -29,11 +31,44 @@ func findFiles(re string) []string {
 	return files
 }
 
-func main() {
+func readFileContentString(filePath string) string {
 	content, err := ioutil.ReadFile("README.md")
 	if err != nil {
 		log.Fatal(err)
-	}
+	}	
 	fmt.Println("File contents: ", string(content))
+	return string(content)
+}
+
+func readFileContentBytes(filePath string) []byte {
+	content, err := ioutil.ReadFile("README.md")
+	if err != nil {
+		log.Fatal(err)
+	}	
+	return content
+}
+
+func hexEncodedUrl(content []byte) string {
+	encodedStr := hex.EncodeToString(content)
+	return fmt.Sprintf("http://www.plantuml.com/plantuml/png/~h%s", encodedStr)
+}
+
+func replaceLineInFile(filePath string, content string, searchString string, replaceString string) {
+	lines := strings.Split(content, "\n")
+
+	for i, line := range lines {
+		if strings.Contains(line, searchString) {
+				lines[i] = replaceString
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err := ioutil.WriteFile(filePath, []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	
 	fmt.Println(findFiles(".*\\.(pu)"))
 }
