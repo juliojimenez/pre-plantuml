4f3e187cd118d3399186d379c852164fae60e0f1
Julio Jimenez <julio@julioj.com>
2021-02-15 13:49:28 -0500

    getting there

--- deflator.go
+++ deflator.go
@@ -30,7 +30,27 @@ func deflateBase64ishEncode(in []byte, iOff int, iLen int) []byte {
 	iEnd := iOff + iLen
 	op := 0
 	for ip < iEnd {
-
+		ip++
+		i0 := in[ip] & 0xff
+		ip++
+		var i1 byte
+		if ip < iEnd {
+			i1 = in[ip] & 0xff
+		} else {
+			i1 = 0
+		}
+		ip++
+		var i2 byte
+		if ip < iEnd {
+			i2 = in[ip] & 0xff
+		} else {
+			i2 = 0
+		}
+		o0 := i0 >> 2
+		o1 := ((i0 & 3) << 4) | (i1 >> 4)
+		o2 := ((i1 & 0xf) << 2) | (i2 >> 6)
+		o3 := i2 & 0x3f
+		
 	}
 	return out
 	fmt.Println(oDataLen)
